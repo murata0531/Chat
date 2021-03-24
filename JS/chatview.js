3a75f719debbe53421c720eda5862aa2caca442f
murata0531 <coless115@gmail.com>
2021-03-25 06:10:22 +0900

    complete : writing firebase

--- JS/chatview.js
+++ JS/chatview.js
@@ -1,40 +1,90 @@
 //button click event
 document.getElementById("send-button").onclick = function() {
     
+    let send_button = document.getElementById("send-button");
+    let database = firebase.database();
 
-    let send_text = document.getElementById("send-text");
-    let send_image = document.getElementById("send-image");
-    let output = document.getElementById('output');
-
-    const database = firebase.database();
     let title = document.getElementsByClassName('chat-header-title');
     let room_id = title[0].id;
     let now = new Date();
+    let send_text = document.getElementById("send-text");
+    let existsMessage;
+    let message_send;
+
+    // message null?
+    if(send_text.value == ''){
+        message_send = "nothing";
+        existsMessage = "no";
+    }else {
+        message_send = send_text.value;
+        existsMessage = "yes";
+    }
+
+    let send_file = document.getElementById("send-file");
+    alert(send_file.files[0]);
+    let image_send;
+    let existsFile;
+
+    // file null?
+    if(send_file.files.length <= 0){
+        file_send = "nothing";
+        existsFile = "no";
+    }else {
+        file_send = 'images/' + room_id + '/' + now + send_file.files[0].name;
+        existsFile = "yes";
+    }
+
+   
 
     let params = new URLSearchParams();
     params.append('post_user_id',my_id);
     params.append('post_chat_id',room_id);
-    params.append('post_message',send_text.value);
-    params.append('post_isfile',send_image);
+    params.append('post_message',message_send);
+    params.append('post_isfile',file_send);
+    params.append('post_existsMessage',existsMessage);
+    params.append('post_existsFile',existsFile);
+
 
     axios.post('http://localhost/Chat/api/Write.php',params)
     .then(response => {
 
+        alert(response.data.result);
         if(response.data.result == "ok"){
+
+            //push database
             database.ref(room_id).push({
                 uid: my_id,
                 icon: my_icon,
                 name: my_name,
-                message: send_text.value,
-                isfile: 'nothing',
+                message: response.data.result_value[0].message,
+                isfile: response.data.result_value[0].isfile,
+                emessage: response.data.result_value[0].exists_message,
+                efile: response.data.result_value[0].exists_file,
                 senddate: response.data.result_value[0].date,
             });
+
+            //exists file : push storage
+            if(response.data.result_value[0].exists_file == "yes"){
+
+                let storageRef = firebase.storage().ref();
+                alert(send_file.files[0]);
+                let uploadTask = storageRef.child(response.data.result_value[0].isfile);
+               
+                uploadTask.put(send_file.files[0]).then(function(snapshot) {
+                alert('Upload successfully');
+                send_file.value = '';
+
+                });
+            }
         }
     })
     .catch(error => {
         alert("error");
     });
     
+    send_text.value = "";
+    send_button.disabled = "disabled";
+    send_button.style.backgroundColor = "gray";
 
 }
 
@@ -91,7 +141,24 @@ function read_firebase(){
         prevTask = prevTask.finally(async () => {
             const v = data.val();
             const k = data.key;
+            let str;
 
+            //my message
+            if(v.uid == my_id){
+
+                if(v.message){
+                    str += '<div class="my-message d-flex flex-column position-relative w-100 mt-5 bg-danger auto">';
+                    str += '<div class="d-flex flex-row position-relative m-3">';
+                    str += '<p class="my-message-date position-relative m-3">' + v.senddate + '</p>';
+                    str += '<img src=".' + my_icon + '" class="my-message-icon ml-3 position-absolute"></div>';
+                    str += '    <p class="my-message-text w-50 text-center bg-white position-relative m-3 p-3 rounded-lg">' + v.message + '</p></div>';
+                    
+                }
+            }
+            
+            str += '';
+            output.innerHTML(str);
+            output.scrollIntoView();
     
         }).catch(function (error) {
 
--- api/Write.php
+++ api/Write.php
@@ -11,15 +11,11 @@ if(isset($_POST['post_user_id']) && isset($_POST['post_chat_id'])) {
 
     // escape
     $userId =  htmlspecialchars($_POST['post_user_id']);
-    $chatId =  htmlspecialchars($_POST["post_chat_id"]);
-
-    if(isset($_POST["post_message"])){
-        $chatMsg =  htmlspecialchars($_POST["post_message"]);
-    }
-
-    if(isset($_POST["post_isfile"])){
-        $chatIsfile =  htmlspecialchars($_POST["post_isfile"]);
-    }
+    $chatId =  htmlspecialchars($_POST['post_chat_id']);
+    $chatMsg =  htmlspecialchars($_POST['post_message']);
+    $chatIsfile =  htmlspecialchars($_POST['post_isfile']);
+    $chatExistsMessage = htmlspecialchars($_POST['post_existsMessage']);
+    $chatExistsFile = htmlspecialchars($_POST['post_existsFile']);
 
     $result;
     $result_value;
@@ -29,52 +25,46 @@ if(isset($_POST['post_user_id']) && isset($_POST['post_chat_id'])) {
         $pdo = new PDO("mysql:host=localhost; dbname=library_am; charset=utf8", 'root', '');
     
         //prepared statement
-        //Exists : message & file
-        if(isset($chatMsg) && isset($chatIsfile)){
-            $stmt = $pdo->prepare('insert into message_logs(chat_id,user_id,message,isfile) values(:cid,:uid,:msg,:isf)');
-        //Exists : message only
-        }else if(isset($chatMsg)) {
-            $stmt = $pdo->prepare('insert into message_logs(chat_id,user_id,message) values(:cid,:uid,:msg)');
-        //Exists : file only
-        }else if(isset($chatIsfile)){
-            $stmt = $pdo->prepare('insert into message_logs(chat_id,user_id,isfile) values(:cid,:uid,:isf)');
-        }
+        $stmt = $pdo->prepare('insert into message_logs(chat_id,user_id,message,isfile,exists_message,exists_file) values(:cid,:uid,:msg,:isf,:emsg,:efile)');
+        
 
         // bind value
         $cid = $chatId;
         $uid = $userId;
-
-        if(isset($chatMsg)){
-            $msg = $chatMsg;
-        }
-        
-        if(isset($chatIsfile)){
-            $isf = $chatIsfile;
-        }
+        $msg = $chatMsg;
+        $isf = $chatIsfile;
+        $emsg = $chatExistsMessage;
+        $efile = $chatExistsFile;
 
         $stmt->bindValue(':cid', $cid, PDO::PARAM_INT);
         $stmt->bindValue(':uid', $uid,PDO::PARAM_INT);
+        $stmt->bindValue(':msg', $msg);
+        $stmt->bindValue(':isf', $isf);
+        $stmt->bindValue(':emsg',$emsg);
+        $stmt->bindValue(':efile',$efile);
 
-        if(isset($msg)){
-            $stmt->bindValue(':msg', $msg);
-        }
-
-        if(isset($isf)){
-            $stmt->bindValue(':isf', $isf);
-        }
-    
         // execute query
-        
         if($stmt->execute()){
         
-            $result = "ok";
             $insert_id = $pdo->lastInsertId();
             $select_sql = "SELECT * FROM message_logs WHERE log_id = '$insert_id'";
 
             $stmt = $pdo->query($select_sql);
-
             $result_value = $stmt->fetchAll(PDO::FETCH_ASSOC);
-            
+
+            //current time
+            $current_date = $result_value[0]['date'];
+
+            //update chat last_modify date to current time
+            $update_sql = "UPDATE chat SET last_modify = '$current_date'";
+            $update_result = $pdo->query($update_sql);
+
+            if($update_result == TRUE){
+                $result = "ok";
+            }else {
+                $result = "bad";
+            }
+
         }else {
             $result = "bad";
             $result_value = "no data";
